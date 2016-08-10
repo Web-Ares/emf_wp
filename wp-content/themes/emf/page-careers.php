6b92fbfb68555ba45bda14c41523535f97c9ad03
aleksandrnikolajcuk <deathstars111>
2016-08-10 10:31:32 +0300

    careers

--- wp-content/themes/emf/page-careers.php
+++ wp-content/themes/emf/page-careers.php
@@ -20,9 +20,9 @@ get_header(); ?>
         </aside>
         <!-- /site__content__aside -->
 
+
         <!-- site__content__inner -->
         <div class="site__content__inner">
-
 
             <!-- tabs -->
             <div class="tabs">
@@ -35,230 +35,52 @@ get_header(); ?>
                             <div class="content content_careers">
 
 
-                                <p>EMF Power Co is looking for career-driven self-starters who desire a job in the very
-                                    competitive electrical services industry. We are looking for high energy individuals who
-                                    are extremely organized, work independently, have strong communication skills, ability
-                                    to multi-task, customer friendly, and ability to work within stressful environments.</p>
-
-                                <p>To apply, please send us your résumé today. Thank you.</p>
+                                    <?php the_field('add_a_text_block'); ?>
 
 
                             </div>
                             <!-- /content -->
 
-                            <!--accordion -->
+                           <?php if( have_rows('add_a_drop_down_list') ):
+
+                            $work = 0; ?>
+
+                            <!-- accordion -->
                             <div class="accordion accordion_careers">
-                                <h2 class="site__title site__title_6">There are 4 jobs in EMF</h2>
 
+                                <h2 class="site__title site__title_6"><?php the_sub_field('title_for_drop_down'); ?></h2>
                                 <dl>
-                                    <dt>Associate Intern, Advanced Manufacturing</dt>
-                                    <dd>
+                                    <?php while ( have_rows('add_a_drop_down_list') ) : the_row();
+                                        $title = get_sub_field('add_a_title_of_item');
+                                        ?>
 
-                                        <h3>Function:</h3>
-                                        <p>Asset Management</p>
+                                        <dt><?php echo $title; ?></dt>
+                                        <dd>
 
-                                        <h3>About Us:</h3>
-                                        <p>EMF is the Digital Industrial Company, transforming industry with software-defined machines and
-                                            solutions that are connected, responsive and predictive. Through our people, leadership develoent,
-                                            services, technology and scale, EMF delivers better outcomes for global customers by speaking the
-                                            language of industry. EMF offers a great work environment, professional development, challenging
-                                            careers, and competitive compensation. </p>
+                                            <!--accordion__content -->
+                                            <div class="accordion__content">
 
-                                        <h3>Role Summary/Purpose:</h3>
-                                        <p>The Associate Intern will assist the EMF Ventures team on a project evaluating investment opport
-                                            unities in a specific sector of Advanced Manufacturing. By the end of the 10 week internship, the
-                                            individual will have developed an understanding of potential investment opportunities associated
-                                            with the sector, and will have summarized the project in a document and presentation for the EMF
-                                            Ventures team.</p>
+                                                <?php the_sub_field('add_a_content_of_item')?>
 
-                                        <h3>Essential Responsibilities:</h3>
-                                        <ul>
-                                            <li>Conduct research on 1-2 key areas of potential investment</li>
-                                            <li>Assist in screening investment opportunities</li>
-                                            <li>Prepare investment committee pitches for potential deals and help present to senior </li>
-                                            <li>Participate in deal due diligence & closing</li>
-                                            <li>Develop and evaluate financial models for potential investments</li>
-                                            <li>Develop diligence data requests and help manage diligence process with target company</li>
-                                            <li>Facilitate negotiation of deal documentation</li>
-                                        </ul>
+                                            </div>
 
-                                        <h3>Qualifications/Requirements:</h3>
-                                        <ul>
-                                            <li>Bachelor's degree</li>
-                                            <li>Pursuing Master's degree</li>
-                                            <li>Proficiency with Microsoft Office Suite including Word, Excel, PowerPoint, and Outlook</li>
-                                        </ul>
+                                            <a href="#" class="btn btn_3 apply" data-value="<?php echo $work; ?>" data-text1="apply" data-text2="applied">
+                                                <span>apply</span>
+                                            </a>
 
-                                        <h3>Qualifications/Requirements:</h3>
-                                        <ul>
-                                            <li>Educational background in engineering, physical sciences, or other relevant field</li>
-                                            <li>Working knowledge of Finance and Valuation concepts</li>
-                                            <li>Demonstrated ability to be a self-starter</li>
-                                            <li>Ability to multitask while maintaining attention to detail</li>
-                                        </ul>
 
-                                        <a href="#" class="btn btn_3 apply" data-value="1" data-text1="apply" data-text2="applied">
-                                            <span>apply</span>
-                                        </a>
+                                        </dd>
 
-                                    </dd>
-                                    <dt>Sr Software Quality Test Engineer</dt>
-                                    <dd>
+                                        <?php $work++;
 
-                                        <h3>Function:</h3>
-                                        <p>Asset Management</p>
-
-                                        <h3>About Us:</h3>
-                                        <p>EMF is the Digital Industrial Company, transforming industry with software-defined machines and
-                                            solutions that are connected, responsive and predictive. Through our people, leadership develoent,
-                                            services, technology and scale, EMF delivers better outcomes for global customers by speaking the
-                                            language of industry. EMF offers a great work environment, professional development, challenging
-                                            careers, and competitive compensation. </p>
-
-                                        <h3>Role Summary/Purpose:</h3>
-                                        <p>The Associate Intern will assist the EMF Ventures team on a project evaluating investment opport
-                                            unities in a specific sector of Advanced Manufacturing. By the end of the 10 week internship, the
-                                            individual will have developed an understanding of potential investment opportunities associated
-                                            with the sector, and will have summarized the project in a document and presentation for the EMF
-                                            Ventures team.</p>
-
-                                        <h3>Essential Responsibilities:</h3>
-                                        <ul>
-                                            <li>Conduct research on 1-2 key areas of potential investment</li>
-                                            <li>Assist in screening investment opportunities</li>
-                                            <li>Prepare investment committee pitches for potential deals and help present to senior </li>
-                                            <li>Participate in deal due diligence & closing</li>
-                                            <li>Develop and evaluate financial models for potential investments</li>
-                                            <li>Develop diligence data requests and help manage diligence process with target company</li>
-                                            <li>Facilitate negotiation of deal documentation</li>
-                                        </ul>
-
-                                        <h3>Qualifications/Requirements:</h3>
-                                        <ul>
-                                            <li>Bachelor's degree</li>
-                                            <li>Pursuing Master's degree</li>
-                                            <li>Proficiency with Microsoft Office Suite including Word, Excel, PowerPoint, and Outlook</li>
-                                        </ul>
-
-                                        <h3>Qualifications/Requirements:</h3>
-                                        <ul>
-                                            <li>Educational background in engineering, physical sciences, or other relevant field</li>
-                                            <li>Working knowledge of Finance and Valuation concepts</li>
-                                            <li>Demonstrated ability to be a self-starter</li>
-                                            <li>Ability to multitask while maintaining attention to detail</li>
-                                        </ul>
-
-                                        <a href="#" class="btn btn_3 apply" data-value="2" data-text1="apply" data-text2="applied">
-                                            <span>apply</span>
-                                        </a>
-
-                                    </dd>
-                                    <dt>Software Lead/Technical Lead</dt>
-                                    <dd>
-
-                                        <h3>Function:</h3>
-                                        <p>Asset Management</p>
-
-                                        <h3>About Us:</h3>
-                                        <p>EMF is the Digital Industrial Company, transforming industry with software-defined machines and
-                                            solutions that are connected, responsive and predictive. Through our people, leadership develoent,
-                                            services, technology and scale, EMF delivers better outcomes for global customers by speaking the
-                                            language of industry. EMF offers a great work environment, professional development, challenging
-                                            careers, and competitive compensation. </p>
-
-                                        <h3>Role Summary/Purpose:</h3>
-                                        <p>The Associate Intern will assist the EMF Ventures team on a project evaluating investment opport
-                                            unities in a specific sector of Advanced Manufacturing. By the end of the 10 week internship, the
-                                            individual will have developed an understanding of potential investment opportunities associated
-                                            with the sector, and will have summarized the project in a document and presentation for the EMF
-                                            Ventures team.</p>
-
-                                        <h3>Essential Responsibilities:</h3>
-                                        <ul>
-                                            <li>Conduct research on 1-2 key areas of potential investment</li>
-                                            <li>Assist in screening investment opportunities</li>
-                                            <li>Prepare investment committee pitches for potential deals and help present to senior </li>
-                                            <li>Participate in deal due diligence & closing</li>
-                                            <li>Develop and evaluate financial models for potential investments</li>
-                                            <li>Develop diligence data requests and help manage diligence process with target company</li>
-                                            <li>Facilitate negotiation of deal documentation</li>
-                                        </ul>
-
-                                        <h3>Qualifications/Requirements:</h3>
-                                        <ul>
-                                            <li>Bachelor's degree</li>
-                                            <li>Pursuing Master's degree</li>
-                                            <li>Proficiency with Microsoft Office Suite including Word, Excel, PowerPoint, and Outlook</li>
-                                        </ul>
-
-                                        <h3>Qualifications/Requirements:</h3>
-                                        <ul>
-                                            <li>Educational background in engineering, physical sciences, or other relevant field</li>
-                                            <li>Working knowledge of Finance and Valuation concepts</li>
-                                            <li>Demonstrated ability to be a self-starter</li>
-                                            <li>Ability to multitask while maintaining attention to detail</li>
-                                        </ul>
-
-                                        <a href="#" class="btn btn_3 apply" data-value="3" data-text1="apply" data-text2="applied">
-                                            <span>apply</span>
-                                        </a>
-
-                                    </dd>
-                                    <dt>Major Component Exchange (MCE) Field Specialist</dt>
-                                    <dd>
-
-                                        <h3>Function:</h3>
-                                        <p>Asset Management</p>
-
-                                        <h3>About Us:</h3>
-                                        <p>EMF is the Digital Industrial Company, transforming industry with software-defined machines and
-                                            solutions that are connected, responsive and predictive. Through our people, leadership develoent,
-                                            services, technology and scale, EMF delivers better outcomes for global customers by speaking the
-                                            language of industry. EMF offers a great work environment, professional development, challenging
-                                            careers, and competitive compensation. </p>
-
-                                        <h3>Role Summary/Purpose:</h3>
-                                        <p>The Associate Intern will assist the EMF Ventures team on a project evaluating investment opport
-                                            unities in a specific sector of Advanced Manufacturing. By the end of the 10 week internship, the
-                                            individual will have developed an understanding of potential investment opportunities associated
-                                            with the sector, and will have summarized the project in a document and presentation for the EMF
-                                            Ventures team.</p>
-
-                                        <h3>Essential Responsibilities:</h3>
-                                        <ul>
-                                            <li>Conduct research on 1-2 key areas of potential investment</li>
-                                            <li>Assist in screening investment opportunities</li>
-                                            <li>Prepare investment committee pitches for potential deals and help present to senior </li>
-                                            <li>Participate in deal due diligence & closing</li>
-                                            <li>Develop and evaluate financial models for potential investments</li>
-                                            <li>Develop diligence data requests and help manage diligence process with target company</li>
-                                            <li>Facilitate negotiation of deal documentation</li>
-                                        </ul>
-
-                                        <h3>Qualifications/Requirements:</h3>
-                                        <ul>
-                                            <li>Bachelor's degree</li>
-                                            <li>Pursuing Master's degree</li>
-                                            <li>Proficiency with Microsoft Office Suite including Word, Excel, PowerPoint, and Outlook</li>
-                                        </ul>
-
-                                        <h3>Qualifications/Requirements:</h3>
-                                        <ul>
-                                            <li>Educational background in engineering, physical sciences, or other relevant field</li>
-                                            <li>Working knowledge of Finance and Valuation concepts</li>
-                                            <li>Demonstrated ability to be a self-starter</li>
-                                            <li>Ability to multitask while maintaining attention to detail</li>
-                                        </ul>
-
-                                        <a href="#" class="btn btn_3 apply" data-value="4" data-text1="apply" data-text2="applied">
-                                            <span>apply</span>
-                                        </a>
-
-                                    </dd>
+                                    endwhile; ?>
                                 </dl>
-
                             </div>
                             <!-- /accordion -->
+                            <?php
+
+
+                            endif; ?>
 
                         </div>
                     </dd>
@@ -278,8 +100,6 @@ get_header(); ?>
             </div>
             <!-- /tabs -->
 
-
-            
         </div>
         <!-- /site__content__inner -->
 
